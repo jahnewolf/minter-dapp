8863fcb52dac128fcf287257443b73086bf4c00f
Jahn E Wolf <jahn.freal@gmail.com>
2022-06-17 06:56:34 +0800

    presaleshit

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,10 +61,10 @@ const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATI
 const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-06-17T00:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-06-17T00:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-06-16T23:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-06-16T23:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x7B5Db1dBDeA603b56921b0ca50cb18a08385d2B6"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
